56c8a6f56abcf7d6c4c91c9439c00d84ab12ab4d
Luis Fernando Marroquin Martinez <luisfer.marroquin@hotmail.com>
2018-05-08 19:53:00 -0600

    Added Turing Machines definition

---
+++ Turing_Machines/Turing_Machines/State.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turing_Machines
+{
+    class State
+    {
+        public byte name;
+        public bool isAcceptance;
+
+        public State(byte name, bool isAcceptance)
+        {
+            this.name = name;
+            this.isAcceptance = isAcceptance; 
+            // TODO EVERYTHING!! :') 
+        }
+    }
+}
